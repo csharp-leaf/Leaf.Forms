85144372b66204a996dd6e847e3ac6ea52dafa15
grandsilence <mixtape774@gmail.com>
2018-07-19 23:19:59 +0300

    New method ConfirmDeleteDirectory

--- Prompts.cs
+++ Prompts.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Leaf.Forms
@@ -21,6 +23,43 @@ namespace Leaf.Forms
                 "Принудительная остановка работы", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
 
             return result == DialogResult.Yes;
+        }
+
+        /// <summary>
+        /// Запрос на удаление директории через MessageBox.
+        /// </summary>
+        /// <param name="name">Имя ресурса который будет очищен, писать с большой буквы</param>
+        /// <param name="directory">Пусть до директории что будет удаляться после подтверждения</param>
+        public static void ConfirmDeleteDirectory(string name, string directory)
+        {
+            string nameLower = name.ToLower();
+            string caption = $"Удалить {nameLower}?";
+
+            bool notExistsOrEmpty = !Directory.Exists(directory) || !Directory.EnumerateFiles(directory).Any();
+            if (notExistsOrEmpty)
+            {
+                MessageBox.Show("Нечего очищать. Очистка уже была произведена", caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Ask
+            var result = MessageBox.Show($"{name} будут очищены, продолжить?", caption,
+                MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+
+            if (result != DialogResult.Yes)
+                return;
+
+            // Delete
+            try
+            {
+                Directory.Delete(directory, true);
+                MessageBox.Show($"{name} очищены!", caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Не удалось очистить {nameLower} из-за ошибки:\n{ex.Message}", caption,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
